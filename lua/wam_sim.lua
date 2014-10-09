5b3ecfb52d4de68e9630ce1bd6a6bee45fdae1a4
Jonathan Bohren <jbo@jhu.edu>
2014-10-09 03:04:32 -0400

    cleaning out old ops files, adding lua scripts

---
+++ lua/wam_sim.lua
@@ -0,0 +1,14 @@
+
+--[[ get ROS global service --]]
+depl:import("rtt_ros")
+ros = gs:provides("ros")
+ros:import("lcsr_barrett")
+
+--[ Set log-level ]--
+rtt.setLogLevel("Warning")
+
+--[[ add lua dir to the lua path --]]
+package.path = ros:find("lcsr_barrett") .. "/lua/?.lua" .. ";" .. package.path
+
+require("lcsr_barrett")
+lcsr_barrett(true)
